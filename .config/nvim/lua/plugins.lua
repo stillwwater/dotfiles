b35a28a41bcac32b5f3c932cde8dfb9077ad4af7
Lucas <stillwwater@gmail.com>
2022-11-26 11:02:23 -0800

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,29 +1,45 @@
-local cmp = require'cmp'
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
+local cmp = require('cmp')
 cmp.setup {
-    completion = {
-        autocomplete = false
-    },
-    snippet = {
-        expand = function(args) end,
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-N>'] = cmp.mapping { i = cmp.mapping.complete() },
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
+    view = { entries = "wildmenu" },
+    confirmation = { completeopt = 'menu,menuone,noinsert' },
+    mapping = {
+        ['<C-Space>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
             select = true,
         },
-    }),
+
+        ['<Tab>'] = function(fallback)
+            if not cmp.select_next_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                    --cmp.confirm({ select = true })
+                else
+                    fallback()
+                end
+            end
+        end,
+
+        ['<S-Tab>'] = function(fallback)
+            if not cmp.select_prev_item() then
+                if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                    cmp.complete()
+                else
+                    fallback()
+                end
+            end
+        end,
+    },
     sources = {
         { name = 'nvim_lsp' },
     },
+    snippet = {},
 }
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-    vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities({ snippetSupport = false })
 
 require('lspconfig').clangd.setup {
     capabilities = capabilities
